fc1ff671f757f421a40a4a54e82eb5c37240fe3f
zhangweiguang <lfzwg@hotmail.com>
2018-09-07 21:59:01 +0800

    gghj

--- src/main/java/org/louis/springmvc/HelloController.java
+++ src/main/java/org/louis/springmvc/HelloController.java
@@ -2,13 +2,32 @@ package org.louis.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/index1111")
     public String hello()
     {
         return "index";
+    }
+
+    @RequestMapping(value = "/pushsse",produces = "text/event-stream")
+    public  @ResponseBody String pushsse()
+    {
+        Random r = new Random();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "data :testing " + r.nextInt() +"\n\n";
+
     }
 }
--- src/main/java/org/louis/springmvc/MvcConfig.java
+++ src/main/java/org/louis/springmvc/MvcConfig.java
@@ -3,17 +3,22 @@ package org.louis.springmvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("org.louis.springmvc")
+@EnableScheduling
 public class MvcConfig  extends WebMvcConfigurerAdapter{
 
     @Bean
@@ -30,10 +35,29 @@ public class MvcConfig  extends WebMvcConfigurerAdapter{
 
     }
 
+    @Bean public MultipartResolver multipartResolver()
+    {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(10000000);
+        return  multipartResolver;
+    }
+
     @Bean
     public DemoInterceptor demoInterceptor()
     {
         return  new DemoInterceptor();
+    }
+
+    @Bean
+    public MyMessageConvert myMessageConvert()
+    {
+        return new  MyMessageConvert();
+    }
+
+    @Override
+    public  void extendMessageConverters(List<HttpMessageConverter<?>> converterList)
+    {
+        converterList.add(myMessageConvert());
     }
 
     @Override
@@ -45,5 +69,14 @@ public class MvcConfig  extends WebMvcConfigurerAdapter{
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(demoInterceptor());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/index").setViewName("/index");
+        registry.addViewController("/toUpload").setViewName("/upload");
+        registry.addViewController("/converter").setViewName("/converter");
+        registry.addViewController("/sse").setViewName("/sse");
     }
 }
